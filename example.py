56d5977d698c51f8a88980504b7448beb69a529f
JK-Sun2331 <1783034576@qq.com>
2025-10-17 17:00:16 +0800

    add KV-cache function

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import llm.LLM as LLM
+import time
 
 model_path = "/data1/hfhub/models--gpt2/snapshots/607a30d783dfa663caf39e06633721c8d4cfcd7e" 
 
@@ -7,11 +8,14 @@ if __name__ == "__main__":
         "I have 10 dollar",
         "I am watching TV"
         ]
-
-    engine = LLM.LLMEngine(model_path,device=1)
-    output = engine.generator(input_text,temperature=0.7,top_k=50)
     
+    engine = LLM.LLMEngine(model_path,device=1)
+    start = time.time()
+    output = engine.generator(input_text,temperature=0.7,top_k=50)
+    end = time.time()
     batch_size = len(input_text)
     
     for i in range(batch_size):
         print(f"\n\noutput_text {i} : {output[i]}\n\n")
+
+    print(end - start)
