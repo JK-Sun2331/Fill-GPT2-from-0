d1f237d6f6e10b90d4433bb3c03fe4adf8ac2ba3
JK-Sun2331 <1783034576@qq.com>
2025-10-17 17:19:34 +0800

    normal tst

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import llm.LLM as LLM
+import time
 
 model_path = "/data1/hfhub/models--gpt2/snapshots/607a30d783dfa663caf39e06633721c8d4cfcd7e" 
 
@@ -9,9 +10,11 @@ if __name__ == "__main__":
         ]
 
     engine = LLM.LLMEngine(model_path,device=1)
+    start = time.time()
     output = engine.generator(input_text,temperature=0.7,top_k=50)
-    
+    end = time.time()
     batch_size = len(input_text)
     
     for i in range(batch_size):
         print(f"\n\noutput_text {i} : {output[i]}\n\n")
+    print(end - start)  #11.56s
